6f39a90bc9cfad5c0a7b6a90c36b3f42a9d6309a
leodedeus <leorodriguesdedeus@gmail.com>
2024-03-24 19:43:46 -0300

    atualizacao

--- app_core/app_core/settings.py
+++ app_core/app_core/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'PASSWORD': 'postgres',
         'PORT': '5432',
         'OPTIONS': {
-            'options': '-c search_path=camadas,public',
+            'options': '-c search_path=public,camadas',
         }
     }
 }
@@ -113,9 +113,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
--- app_core/pi_webgis/admin.py
+++ app_core/pi_webgis/admin.py
@@ -1,23 +1,26 @@
 from django.contrib import admin
 from leaflet.admin import LeafletGeoAdmin
-from django.contrib.gis import admin
-from .models import Escolaspublicas, Loteexistente, Regiaoadministrativa
+#from django.contrib.gis import admin
 #from django.contrib.gis.admin import OSMGeoAdmin
+from .models import Escolaspublicas, Loteexistente, Regiaoadministrativa
 
 
 # Register your models here.
 
 class EscolaspublicasAdmin(LeafletGeoAdmin):
+#class EscolaspublicasAdmin(admin.GISModelAdmin):
     list_display = ('cod_entidade', 'nome_escola')
     search_fields = ('cod_entidade', 'nome_escola')
     #list_filter = ('cod_entidade',)
     #fields = ('cod_entidade', 'nome_escola', 'geom')
 
 class RegiaoadministrativaAdmin(LeafletGeoAdmin):
+#class RegiaoadministrativaAdmin(admin.GISModelAdmin):
     list_display = ('ra_cira', 'ra_nome', 'ra_codigo')
     search_fields = ('ra_nome', 'ra_cira')
 
 class LoteexistenteAdmin(LeafletGeoAdmin):
+#class LoteexistenteAdmin(admin.GISModelAdmin):
     list_display = ('ct_ciu', 'lt_enderec')
     search_fields = ('ct_ciu', 'lt_enderec')
 
