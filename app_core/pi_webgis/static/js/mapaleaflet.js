e6a0cb8b7317efb3d0651c21c6cd390e71a363fc
leodedeus <leorodriguesdedeus@gmail.com>
2024-04-13 19:15:47 -0300

    ajuste funcao busca endereco

--- app_core/pi_webgis/static/js/mapaleaflet.js
+++ app_core/pi_webgis/static/js/mapaleaflet.js
@@ -17,50 +17,52 @@ document.addEventListener('DOMContentLoaded', function () {
         layers: 'TOPO-WMS'
     });
     
-    function adicionarMarcadorEndereco(latitude, longitude, endereco, tipoLocal) {
-        var popupContent = '<b>Endereço:</b> ' + endereco + '<br>';
-        if (tipoLocal) {
-            popupContent += '<b>Tipo:</b> ' + tipoLocal + '<br>';
-        }
-        var marcador = L.marker([latitude, longitude]).addTo(map);
-        marcador.bindPopup(popupContent).openPopup();
-    }
+    // Variável para armazenar a referência do marcador de local atual
+    var marcadorEndereco;
 
-    /*
     // Função para fazer a busca de endereço e adicionar marcador no mapa
     function buscarEndereco(endereco) {
         fetch('/busca_endereco/?address=' + encodeURIComponent(endereco))
             .then(response => response.json())
             .then(data => {
                 if (data.latitude && data.longitude) {
-                    adicionarMarcadorEndereco(data.latitude, data.longitude, data.endereco, data.tipoLocal);
-                    map.setView([data.latitude, data.longitude], 15);
-                } else {
-                    console.error('Endereço não encontrado:', data.error);
-                }
-            })
-            .catch(error => console.error('Erro:', error));
-    }
-    */
+                    // Remove o marcador anterior, se existir
+                    if (marcadorEndereco) {
+                        map.removeLayer(marcadorEndereco);
+                    }
 
-    function buscarEndereco(endereco) {
-        fetch('/busca_endereco/?address=' + encodeURIComponent(endereco))
-            .then(response => response.json())
-            .then(data => {
-                if (data.latitude && data.longitude) {
+                    // Adiciona o novo marcador e popup
                     adicionarMarcadorEndereco(data.latitude, data.longitude, data.endereco, data.tipoLocal);
-                    
+                
                     // Define o zoom do mapa para um nível específico
                     map.setZoom(17);
-    
+
                     // Define o centro do mapa para as coordenadas do marcador
                     map.setView([data.latitude, data.longitude]);
+
+                    // Atualiza a referência para o novo marcador
+                    marcadorEndereco = marcador;
                 } else {
                     console.error('Endereço não encontrado:', data.error);
                 }
             })
             .catch(error => console.error('Erro:', error));
     }
+
+    //função usada para criar o marcador de endereço
+    function adicionarMarcadorEndereco(latitude, longitude, endereco, tipoLocal) {
+        var popupContent = '<b>Endereço:</b> ' + endereco + '<br>';
+        if (tipoLocal) {
+            popupContent += '<b>Tipo:</b> ' + tipoLocal + '<br>';
+        }
+        marcadorEndereco = L.marker([latitude, longitude]).addTo(map);
+        //marcadorEndereco.bindPopup(popupContent).openPopup();
+        marcadorEndereco.bindPopup(popupContent).on('popupclose', function() {
+            // Remove o marcador quando o popup é fechado
+            map.removeLayer(marcadorEndereco);
+        }).openPopup();
+    }
+
     // Função para criar camada de escolas a partir de GeoJSON
     function criarCamadaEscolasGeojson(url) {
         return fetch(url)
